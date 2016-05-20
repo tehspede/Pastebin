4594d10b2c267ae27676f889f6f2823913341231
Aleksi Gold <aleksi@kulta.us>
2016-05-20 05:45:40 +0300

    Remove unneeded bodyParser

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var app = express();
 var bodyParser = require("body-parser");
 var routes = require('./routes.js');
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client'));
